04709bc540f953af4f42095864b51bde83fd6e1c
LukaszKot <lukaszkot03@gmail.com>
2023-12-30 20:20:47 +0100

    Finished basics.

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RecipeItemComponent } from './recipe-item/recipe-item.component';
+import { Recipe } from '../recipe.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-list',
   standalone: true,
-  imports: [RecipeItemComponent],
+  imports: [RecipeItemComponent, CommonModule],
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simple a test.', 'https://familystylefood.com/wp-content/uploads/2023/01/Creamy-Dijon-chicken-hero-2.jpg'),
+    new Recipe('A Test Recipe', 'This is simple a test.', 'https://familystylefood.com/wp-content/uploads/2023/01/Creamy-Dijon-chicken-hero-2.jpg'),
+  ];
 }
